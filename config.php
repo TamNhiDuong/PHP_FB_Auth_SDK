d83f7cfaef4280c2c7d7a65ca726a48055006b71
TamNhiDuong <yanxinyee@gmail.com>
2020-08-11 12:57:35 +0300

    config own ID & password

--- config.php
+++ config.php
@@ -3,8 +3,8 @@ session_start();
 require_once('Facebook/autoload.php');
 
 $FBObject = new \Facebook\Facebook([
-	'app_id' => '754774841964305',
-	'app_secret' => 'de5560bfbbf5d91eebd51e3d9f1b8bf1',
+	'app_id' => '',
+	'app_secret' => '',
 	'default_graph_version' => 'v2.10'
 ]);
 
